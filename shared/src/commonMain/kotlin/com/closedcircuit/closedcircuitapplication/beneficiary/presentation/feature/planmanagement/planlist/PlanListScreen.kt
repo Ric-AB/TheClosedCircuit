36a360928731fc8bbacb36cb6d9071f4ef151498
Richard Bajomo <richardbajomo@gmail.com>
2024-08-02 22:02:10 +0100

    implement last fund request WIP

--- shared/src/commonMain/kotlin/com/closedcircuit/closedcircuitapplication/beneficiary/presentation/feature/planmanagement/planlist/PlanListScreen.kt
+++ shared/src/commonMain/kotlin/com/closedcircuit/closedcircuitapplication/beneficiary/presentation/feature/planmanagement/planlist/PlanListScreen.kt
@@ -7,17 +7,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -40,6 +41,7 @@ import com.closedcircuit.closedcircuitapplication.common.presentation.component.
 import com.closedcircuit.closedcircuitapplication.common.presentation.theme.horizontalScreenPadding
 import com.closedcircuit.closedcircuitapplication.common.presentation.theme.verticalScreenPadding
 import com.closedcircuit.closedcircuitapplication.resources.SharedRes
+import dev.icerock.moko.resources.compose.painterResource
 import dev.icerock.moko.resources.compose.stringResource
 import org.koin.core.component.KoinComponent
 
@@ -57,99 +59,116 @@ internal class PlanListScreen : Screen, KoinComponent {
             navigateToPlanDetailsScreen = { plan -> navigator.push(PlanDetailsScreen(plan)) }
         )
     }
-}
 
-@Composable
-private fun ScreenContent(
-    state: PlanListUiState,
-    goBack: () -> Unit,
-    navigateToCreatePlanScreen: () -> Unit,
-    navigateToPlanDetailsScreen: (Plan) -> Unit
-) {
-    BaseScaffold(
-        topBar = {
-            DefaultAppBar(
-                title = stringResource(SharedRes.strings.plans),
-                mainAction = goBack
-            )
-        },
-        floatingActionButton = { PlansExtendedFab(onClick = navigateToCreatePlanScreen) }
-    ) { innerPadding ->
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-                .padding(horizontal = horizontalScreenPadding, vertical = verticalScreenPadding)
-        ) {
-            items(state.plans) { plan ->
-                PlanCard(
-                    modifier = Modifier.fillMaxWidth(),
-                    plan = plan,
-                    onClick = { navigateToPlanDetailsScreen(plan) }
+    @Composable
+    private fun ScreenContent(
+        state: PlanListUiState,
+        goBack: () -> Unit,
+        navigateToCreatePlanScreen: () -> Unit,
+        navigateToPlanDetailsScreen: (Plan) -> Unit
+    ) {
+        BaseScaffold(
+            topBar = {
+                DefaultAppBar(
+                    title = stringResource(SharedRes.strings.plans),
+                    mainAction = goBack
                 )
-            }
-        }
-    }
-}
-
-@Composable
-private fun PlanCard(modifier: Modifier = Modifier, plan: Plan, onClick: () -> Unit) {
-    OutlinedCard(modifier = modifier, onClick = onClick) {
-        Column(modifier = Modifier.padding(horizontal = 12.dp, vertical = 16.dp)) {
-            Avatar(
-                imageUrl = plan.avatar.value,
-                size = DpSize(50.dp, 50.dp)
-            )
-
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = plan.name,
-                style = MaterialTheme.typography.titleMedium,
-                fontWeight = FontWeight.SemiBold
-            )
-
-            Spacer(modifier = Modifier.height(4.dp))
-            BodyText(text = plan.description)
-
-            Spacer(modifier = Modifier.height(16.dp))
-            TaskLinearProgress(
-                progress = plan.fundsRaisedPercent.toFloat(),
-                label = stringResource(
-                    SharedRes.strings.x_percent_funds_raised_label,
-                    plan.fundsRaisedPercent
-                )
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-            TaskLinearProgress(
-                modifier = Modifier.fillMaxWidth(),
-                progress = plan.tasksCompletedPercent.toFloat(),
-                label = stringResource(
-                    SharedRes.strings.x_percent_tasks_completed_label,
-                    plan.tasksCompletedPercent
-                )
-            )
-
-            if (plan.lastFundRequest != null) {
-                Spacer(Modifier.height(12.dp))
-                IconButton(onClick = {}, modifier = Modifier.align(Alignment.End)) {
-                    Icon(
-                        imageVector = Icons.Default.Share,
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.primary
+            },
+            floatingActionButton = { PlansExtendedFab(onClick = navigateToCreatePlanScreen) }
+        ) { innerPadding ->
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(horizontal = horizontalScreenPadding, vertical = verticalScreenPadding)
+            ) {
+                items(state.plans) { plan ->
+                    PlanCard(
+                        modifier = Modifier.fillMaxWidth(),
+                        plan = plan,
+                        onClick = { navigateToPlanDetailsScreen(plan) }
                     )
                 }
             }
         }
     }
+
+    @Composable
+    private fun PlanCard(modifier: Modifier = Modifier, plan: Plan, onClick: () -> Unit) {
+        OutlinedCard(modifier = modifier, onClick = onClick) {
+            Column(modifier = Modifier.padding(horizontal = 12.dp, vertical = 16.dp)) {
+                Avatar(
+                    imageUrl = plan.avatar.value,
+                    size = DpSize(50.dp, 50.dp)
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = plan.name,
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+                BodyText(text = plan.description)
+
+                Spacer(modifier = Modifier.height(16.dp))
+                TaskLinearProgress(
+                    progress = plan.fundsRaisedPercent.toFloat(),
+                    label = stringResource(
+                        SharedRes.strings.x_percent_funds_raised_label,
+                        plan.fundsRaisedPercent
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+                TaskLinearProgress(
+                    modifier = Modifier.fillMaxWidth(),
+                    progress = plan.tasksCompletedPercent.toFloat(),
+                    label = stringResource(
+                        SharedRes.strings.x_percent_tasks_completed_label,
+                        plan.tasksCompletedPercent
+                    )
+                )
+
+                if (plan.lastFundRequest != null) {
+                    CopyButton(modifier = Modifier.align(Alignment.End), onClick = {})
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun CopyButton(modifier: Modifier, onClick: () -> Unit) {
+        Spacer(Modifier.height(12.dp))
+        TextButton(
+            onClick = onClick,
+            modifier = modifier,
+            colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colorScheme.primary)
+        ) {
+            Text(
+                text = stringResource(SharedRes.strings.copy_link_label),
+                style = MaterialTheme.typography.bodySmall
+            )
+
+            Spacer(Modifier.width(4.dp))
+            Icon(
+                painter = painterResource(SharedRes.images.ic_copy),
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+
+    @Composable
+    private fun PlansExtendedFab(onClick: () -> Unit) {
+        ExtendedFloatingActionButton(
+            onClick = onClick,
+            text = { Text(text = stringResource(SharedRes.strings.new_plan)) },
+            icon = { Icon(imageVector = Icons.Default.Add, contentDescription = null) }
+        )
+    }
 }
 
-@Composable
-private fun PlansExtendedFab(onClick: () -> Unit) {
-    ExtendedFloatingActionButton(
-        onClick = onClick,
-        text = { Text(text = stringResource(SharedRes.strings.new_plan)) },
-        icon = { Icon(imageVector = Icons.Default.Add, contentDescription = null) }
-    )
-}
+
