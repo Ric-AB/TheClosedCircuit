5f63f3a476200e4f323bd8b87e0e2ec889325236
Richard Bajomo <richardbajomo@gmail.com>
2024-08-14 15:54:49 +0100

    update kstore to fix crash on physical iOS device

--- shared/src/androidMain/kotlin/com/closedcircuit/closedcircuitapplication/core/storage/KStore.kt
+++ shared/src/androidMain/kotlin/com/closedcircuit/closedcircuitapplication/core/storage/KStore.kt
@@ -5,14 +5,16 @@ import com.closedcircuit.closedcircuitapplication.common.domain.session.Session
 import com.closedcircuit.closedcircuitapplication.common.domain.user.User
 import io.github.xxfast.kstore.KStore
 import io.github.xxfast.kstore.file.storeOf
+import okio.Path.Companion.toPath
 
 actual val appSettingsStore: KStore<AppSettings> by lazy {
-    storeOf(Storage.appSettingsFile, AppSettings())
+    storeOf(Storage.appSettingsFile.toPath(), AppSettings())
 }
+
 actual val sessionStore: KStore<Session> by lazy {
-    storeOf(Storage.sessionFile)
+    storeOf(Storage.sessionFile.toPath())
 }
 
 actual val userStore: KStore<User> by lazy {
-    storeOf(Storage.userFile)
+    storeOf(Storage.userFile.toPath())
 }
--- shared/src/iosMain/kotlin/com/closedcircuit/closedcircuitapplication/Main.ios.kt
+++ shared/src/iosMain/kotlin/com/closedcircuit/closedcircuitapplication/Main.ios.kt
@@ -3,18 +3,21 @@ package com.closedcircuit.closedcircuitapplication
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.interop.LocalUIViewController
 import androidx.compose.ui.window.ComposeUIViewController
-import com.closedcircuit.closedcircuitapplication.core.storage.storageDir
 import com.closedcircuit.closedcircuitapplication.common.presentation.LocalImagePicker
 import com.closedcircuit.closedcircuitapplication.common.presentation.LocalShareHandler
 import com.closedcircuit.closedcircuitapplication.common.presentation.util.IOSShareHandler
 import com.closedcircuit.closedcircuitapplication.common.presentation.util.ImagePickerFactory
-import platform.Foundation.NSHomeDirectory
+import com.closedcircuit.closedcircuitapplication.core.storage.storageDir
+import io.github.xxfast.kstore.file.utils.DocumentDirectory
+import io.github.xxfast.kstore.utils.ExperimentalKStoreApi
+import platform.Foundation.NSFileManager
 import platform.UIKit.UIScreen
 import platform.UIKit.UIUserInterfaceStyle
 
 
+@OptIn(ExperimentalKStoreApi::class)
 fun MainViewController() = ComposeUIViewController {
-    storageDir = NSHomeDirectory()
+    storageDir = NSFileManager.defaultManager.DocumentDirectory?.relativePath.orEmpty()
     val isDarkTheme =
         UIScreen.mainScreen.traitCollection.userInterfaceStyle == UIUserInterfaceStyle.UIUserInterfaceStyleDark
     CompositionLocalProvider(
